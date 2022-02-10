b57a38900d09b98c500b3095b2863185fcd28740
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-10 11:55:37 -0300

    Task 11 of project done

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,39 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Research: Function which returns largest/smallest in Array : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+function generatePhoneNumber(arrayNum) {
+  if (arrayNum.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (Math.max(...arrayNum) > 9 || Math.min(...arrayNum) < 0) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  for (let number of arrayNum) {
+    let count = 0;
+    for (let number1 of arrayNum) {
+      if (number1 === number) {
+        count = count + 1;
+      }
+    }
+    if (count > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let phoneNumString = '';
+  for (let i = 0; i < arrayNum.length; i += 1) {
+    if (i === 0) {
+      phoneNumString = phoneNumString + '(';
+      phoneNumString = phoneNumString + arrayNum[i];
+    } else if (i === 1) {
+      phoneNumString = phoneNumString + arrayNum[i];
+      phoneNumString = phoneNumString + ') ';
+    } else if (i === 6) {
+      phoneNumString = phoneNumString + arrayNum[i];
+      phoneNumString = phoneNumString + '-';
+    } else {
+      phoneNumString = phoneNumString + arrayNum[i];
+    }
+  }
+  return phoneNumString;
 }
 
 // Desafio 12
